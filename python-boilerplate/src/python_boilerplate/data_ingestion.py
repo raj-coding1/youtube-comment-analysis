61ac86bfdcc174cde5ae45ccac346c66bdd2abaf
raj-coding1 <rkgarai17@gmail.com>
2025-12-02 21:26:02 +0530

    hello

--- python-boilerplate/src/python_boilerplate/data_ingestion.py
+++ python-boilerplate/src/python_boilerplate/data_ingestion.py
@@ -3,6 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+import os
+import yaml
+
+#load the parameter
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+print(curr_dir)
+params_path = os.path.join(curr_dir, '..','..','params.yaml')
+
+with open(os.path.realpath(params_path), 'r') as file:
+    params = yaml.safe_load(file)
 
 #load the data
 df = pd.read_csv('https://raw.githubusercontent.com/Himanshu-1703/reddit-sentiment-analysis/refs/heads/main/data/reddit.csv')
@@ -16,16 +26,24 @@ df.drop_duplicates(inplace = True)
 #remove blank values
 df = df[~(df['clean_comment'].str.strip() == '')]
 
-#divide the data into X and y
-X = df['clean_comment']
-y = df['category']
+# #divide the data into X and y
+# X = df['clean_comment']
+# y = df['category']
 
 #split the data
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=0.2,
+train_data, test_data = train_test_split(
+    df, 
+    test_size= params['data_ingestion']['test_size'],
     random_state=42
 )
+print(params)
+path = os.path.realpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..','..','data\\tests'))
+paths = os.path.join(path,'train_data.csv')
+# print(path)
+# print(paths)
+pathss = os.path.join(path,'test_data.csv')
+train_data.to_csv(paths,index = False)
+test_data.to_csv(pathss,index = False)
 
 
 
